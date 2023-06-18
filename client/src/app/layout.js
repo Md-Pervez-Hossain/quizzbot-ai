f249c52e75b773e2396c160c9e85befa12d860e0
Farhat Mahi <farhatmahi007@gmail.com>
2023-06-18 20:31:05 +0600

    Footer initial design

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,14 +1,17 @@
 import AuthProvider from "@/context/AuthProvider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Shared/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <AuthProvider>
         <body className={inter.className}>{children}</body>
+        <Footer />
       </AuthProvider>
     </html>
   );
