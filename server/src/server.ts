b975c194c12308be682acc3ddafbb949f1280b35
Farhat Mahi <farhatmahi007@gmail.com>
2023-06-20 17:30:54 +0600

    [DONE] added eslint and prettier checker

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("🚀 database connection established");
+    await mongoose.connect(config.database_url as string)
+    console.log('🚀 database connection established')
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`);
-    });
+      console.log(`App listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("Error connecting to database");
+    console.log('Error connecting to database')
   }
 }
 
-main();
+main()
